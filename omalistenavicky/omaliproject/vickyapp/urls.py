c3b69ba63291e0401970ea0dd0e231d8eb8dcd08
Omali stena vicky <omalstena55@gmail.com>
2026-06-05 15:56:53 +0300

    omalistenavicky

--- omalistenavicky/omaliproject/vickyapp/urls.py
+++ omalistenavicky/omaliproject/vickyapp/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
 from . import views
 
-
-from django.urls import path
-from . import views
-
 urlpatterns = [
-    path('', views.index, name='index'),                    # Index page
-    path('home/', views.home, name='home'),                # Home page with video list
-    path('upload/', views.upload_video, name='upload_video'),  # Upload video page
-    path('video/', views.video_page, name='video_page'),    # Video page (video.html)
+    path('', views.landing, name='landing'),
+    path('videos/', views.video_list, name='video_list'),
+    path('upload/', views.upload_video, name='upload_video'),
+    path('video/<int:pk>/', views.video_detail, name='video_detail'),
 ]
